04cbfa5e8a34239949eed902d8960438468dccac
pitsomnk <pitsomnk@gmail.com>
2024-10-29 23:52:21 +0200

    new nextjs app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+
+const  Home =()=> {
+  return (
+    <h1 className="text-3xl text-violet-700 font-black ">This is the next phase of building web applications </h1>
+  );
+}
+
+
+export default Home
